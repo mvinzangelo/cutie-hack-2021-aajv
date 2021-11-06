b7bab3c6e52af2ab6cc1de67a6330f79228ad601
jecndlria <jecndlria@gmail.com>
2021-11-06 10:59:34 -0700

    as

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-#define DEBUG 0
+#define DEBUG 1
 
 #if DEBUG
   #define LOG(x) (cout << x << endl)
@@ -14,11 +14,20 @@ using namespace std;
 
 int main() {
   LOG("hello world.");
-  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+  sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML window", sf::Style::Fullscreen);
+  window.setFramerateLimit(60);
   sf::Texture texture;
   texture.loadFromFile("me.png");
   sf::Sprite sprite(texture);
   while (window.isOpen()) {
+    sf::Event event; 
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) 
+        window.close();
+      else if (event.type == sf::Event::MouseButtonPressed)
+        LOG("mouse");
+    }
+    window.clear(); 
     window.draw(sprite);
     window.display();
   }
